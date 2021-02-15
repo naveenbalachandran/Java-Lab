74f6ff615f2620159535bddaac1c6ebd62a6705b
naveenbalachandran <bnaveen360@gmail.com>
2021-02-15 19:49:00 +0530

    Update triangle.java

--- JavaLAB2/triangle.java
+++ JavaLAB2/triangle.java
@@ -12,7 +12,7 @@ public class triangle {
 
     int area(int x, int y, int z)
     {
-        return (int) (0.5 * 3 * 4);
+        return (int) (0.5 * x * y);
     }
     public static void main(String[] args) {
         triangle obj = new triangle();
